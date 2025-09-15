adbe0b5f30ad6387230558c1503321267cdffe82
michael_varaldo <michael_varaldo@hotmail.com>
2025-09-15 17:52:31 -0300

    feat: implementa API de upload e componente UploadImage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import Upload from './components/Upload';
+import UploadImage from './components/UploadImage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <Upload />
-    </div>
+    <main className="flex justify-center items-center min-h-screen bg-gray-100">
+      <UploadImage />
+    </main>
   );
 }
 
